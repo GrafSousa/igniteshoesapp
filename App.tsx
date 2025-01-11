a8023d6853ceae0ae57c4b3fbe14c71181ab99bf
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2025-01-11 13:09:37 -0300

    chore: add onesignal config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OneSignal } from 'react-native-onesignal';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
 
@@ -9,14 +14,16 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+OneSignal.initialize(process.env.ONESIGNAL_APP_ID ?? '');
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
+        barStyle='light-content'
+        backgroundColor='transparent'
         translucent
       />
       <CartContextProvider>
